b493a07cf19cfbeb393d810eed56572b0af63f8e
kaf-lamed-beyt <belac335@gmail.com>
2023-02-16 16:40:57 +0100

    chore: update webhook receiver

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,8 +1,27 @@
 import { createNodeMiddleware, createProbot } from "probot";
+import { verifyAndReceive } from "@octokit/webhooks";
 
 const app = require("../../../index");
 
 module.exports = createNodeMiddleware(app, {
   probot: createProbot(),
   webhooksPath: "/api/github/webhooks",
+
+  async onUnhandledRequest(req, res) {
+    const { headers, method } = req;
+
+    if (method !== "POST") {
+      res.status(404).send("Not found");
+      return;
+    }
+
+    const payload = await verifyAndReceive({
+      secret: process.env.WEBHOOK_SECRET,
+      headers,
+      payload: req.body,
+    });
+
+    console.log(payload);
+    res.status(200).send("OK");
+  },
 });
