f6e32ece4d2e8f9ec3c76944106d54440196b56d
kaf-lamed-beyt <belac335@gmail.com>
2023-04-03 12:54:12 +0100

    chore: array of responses for both cases

--- index.js
+++ index.js
@@ -45,14 +45,39 @@ module.exports = (app) => {
       username: context.payload.sender.login,
     });
 
+    const totalStars = `${REPO} has ${
+      STARGAZERS > 1 ? `${STARGAZERS} stars` : `${STARGAZERS} star`
+    } now`;
+
+    let responsesForWhenAStarIsAdded = [
+      `Thank you so much for starring this repo, @${USER} :pray:, this means a lot! \n\n ${totalStars}`,
+      `Wow! Thanks for the star, @${USER}! :star2: ${totalStars}`,
+      `Thank you for showing your support by starring this repository, @${USER}! :raised_hands: ${totalStars}`,
+      `We appreciate your interest in this repository, @${USER}! Thank you for the star! :star: ${totalStars}`,
+      `Thanks for the star, @${USER}! It means a lot to us! :pray: ${totalStars}`,
+      `You just made our day, @${USER}! Thanks for the star! :heart_eyes: ${totalStars}`,
+    ];
+
+    let responsesForWhenAStarIsRemoved = [
+      `We're sad to see you go, @${USER}. Thanks for your support while you were here! :cry: \n\n ${totalStars}`,
+      `Thanks for your interest in this repository, @${USER}. We hope you'll come back soon! :wave: \n\n ${totalStars}`,
+      `Sorry to see you unstar the repository, @${USER}. We appreciate your support! :pray: \n\n ${totalStars}`,
+      `Thanks for the time you spent with us, @${USER}. We hope to see you again soon! :smile: \n\n ${totalStars}`,
+      `We'll miss you, @${USER}! Thanks for your support while you were here! :sob: \n\n ${totalStars}`,
+      `@${USER} just unstarred this repository :sob: :sob: \n\n ${totalStars};`,
+    ];
+
+    const randomIsStarred = Math.floor(
+      Math.random() * responsesForWhenAStarIsAdded.length
+    );
+    const randomIsStarRemoved = Math.floor(
+      Math.random() * responsesForWhenAStarIsRemoved.length
+    );
+
     const commentBody =
       context.name === "star" && context.payload.action === "created"
-        ? `Thank you so much for starring this repo, @${USER} :pray:, this means a lot! \n\n ${REPO} has ${
-            STARGAZERS > 1 ? `${STARGAZERS} stars` : `${STARGAZERS} star`
-          } now`
-        : `@${USER} just unstarred this repository :sob: :sob: \n\n ${REPO} has ${
-            STARGAZERS > 1 ? `${STARGAZERS} stars` : `${STARGAZERS} star`
-          } now`;
+        ? randomIsStarred
+        : randomIsStarRemoved;
 
     return context.octokit.issues.createComment({
       owner: OWNER,
