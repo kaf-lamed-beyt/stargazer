5b5eb07388c050d0e5e0fb5e4b43b9a9c5376946
kaf-lamed-beyt <belac335@gmail.com>
2023-04-04 14:07:53 +0100

    chore: unsubscribe info

--- index.js
+++ index.js
@@ -46,48 +46,50 @@ module.exports = (app) => {
     });
 
     const totalStars = `${REPO} has ${
-      STARGAZERS > 1 ? `${STARGAZERS} stars` : `${STARGAZERS} star`
+      STARGAZERS > 1 ? `**${STARGAZERS} stars**` : `**${STARGAZERS} star**`
     } now`;
+
+    const unsubscribe = `PS: don't forget to unsubscribe from this thread. \n If you don't &mdash; whenever anyone stars this repo, you'll get a mail, notifying you, because you were mentioned previously.`;
 
     let responsesForWhenAStarIsAdded = [
       {
-        message: `Thank you so much for starring this repo, @${USER} :pray:, this means a lot! \n\n ${totalStars}`,
+        message: `Thank you so much for starring this repo, @${USER} :pray:, this means a lot! \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `Wow! Thanks for the star, @${USER}! :star2: \n\n ${totalStars}`,
+        message: `Wow! Thanks for the star, @${USER}! :star2: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `Thank you for showing your support by starring this repository, @${USER}! :raised_hands: \n\n ${totalStars}`,
+        message: `Thank you for showing your support by starring this repository, @${USER}! :raised_hands: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `We appreciate your interest in this repository, @${USER}! Thank you for the star! :star: \n\n ${totalStars}`,
+        message: `We appreciate your interest in this repository, @${USER}! Thank you for the star! :star: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `Thanks for the star, @${USER}! It means a lot to us! :pray: \n\n ${totalStars}`,
+        message: `Thanks for the star, @${USER}! It means a lot to us! :pray: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `You just made our day, @${USER}! Thanks for the star! :heart_eyes: \n\n ${totalStars}`,
+        message: `You just made our day, @${USER}! Thanks for the star! :heart_eyes: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
     ];
 
     let responsesForWhenAStarIsRemoved = [
       {
-        message: `We're sad to see you go, @${USER}. Thanks for your support while you were here! :cry: \n\n ${totalStars}`,
+        message: `We're sad to see you go, @${USER}. Thanks for your support while you were here! :cry: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `Thanks for your interest in this repository, @${USER}. We hope you'll come back soon! :wave: \n\n ${totalStars}`,
+        message: `Thanks for your interest in this repository, @${USER}. We hope you'll come back soon! :wave: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `Sorry to see you unstar the repository, @${USER}. We appreciate your support! :pray: \n\n ${totalStars}`,
+        message: `Sorry to see you unstar the repository, @${USER}. We appreciate your support! :pray: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `Thanks for the time you spent with us, @${USER}. We hope to see you again soon! :smile: \n\n ${totalStars}`,
+        message: `Thanks for the time you spent with us, @${USER}. We hope to see you again soon! :smile: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `We'll miss you, @${USER}! Thanks for your support while you were here! :sob: \n\n ${totalStars}`,
+        message: `We'll miss you, @${USER}! Thanks for your support while you were here! :sob: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
       {
-        message: `@${USER} just unstarred this repository :sob: :sob: \n\n ${totalStars};`,
+        message: `@${USER} just unstarred this repository :sob: :sob: \n\n ${totalStars} \n\n ${unsubscribe}`,
       },
     ];
 
